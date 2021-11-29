bac56b7f30ab79633e578f5285ce3e5ddc3a19ca
Sorin <jordache_sor@yahoo.com>
2021-11-29 15:02:03 +0200

    Add project files.

---
+++ Activity Tracker WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Activity_Tracker_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
