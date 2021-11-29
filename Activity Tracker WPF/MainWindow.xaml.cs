8e318546dcb3f9f29272e0b4b67703cda3fec81c
Sorin <jordache_sor@yahoo.com>
2021-11-29 17:44:28 +0200

    added file loading

--- Activity Tracker WPF/MainWindow.xaml.cs
+++ Activity Tracker WPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MahApps.Metro.Controls;
+﻿using Activity_Tracker_WPF.Resources;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Activity_Tracker_WPF
 {
@@ -7,9 +10,52 @@ namespace Activity_Tracker_WPF
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private string data;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadFile(Settings.Default.LastPath);
+        }
+
+        private void LoadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                UpdateLastPath(path);
+                UpdateFileName(path);
+
+                data = File.ReadAllText(path);
+                dataTextBox.Text = data;
+            }
+        }
+
+        private void UpdateLastPath(string newPath)
+        {
+            if (Settings.Default.LastPath != newPath)
+            {
+                Settings.Default.LastPath = newPath;
+                Settings.Default.Save();
+            }
+        }
+
+        private void UpdateFileName(string newPath)
+        {
+            fileNameLabel.Content = Path.GetFileNameWithoutExtension(newPath);
+        }
+
+        private void BrowseToFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Title = "Open Activity File";
+            openFileDialog.DefaultExt = ".txt";
+            openFileDialog.Filter = "Text Documents (.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                LoadFile(openFileDialog.FileName);
+            }
         }
     }
 }
