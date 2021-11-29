454f30bd0c4ebc05a4ba817485fc44fb07a08f10
Sorin <jordache_sor@yahoo.com>
2021-11-29 22:30:44 +0200

    added keyboard shortcuts

--- Activity Tracker WPF/MainWindow.xaml.cs
+++ Activity Tracker WPF/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using Activity_Tracker_WPF.Resources;
+﻿using Activity_Tracker_WPF.Models;
+using Activity_Tracker_WPF.Resources;
 using MahApps.Metro.Controls;
 using Microsoft.Win32;
+using System;
 using System.IO;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Activity_Tracker_WPF
 {
@@ -10,13 +14,67 @@ namespace Activity_Tracker_WPF
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        public DelegateCommand InsertDayCommand { get; }
+        public DelegateCommand InsertTaskCommand { get; }
+
         private string data;
+        //private static string newDayTemplate = "[29-11-2021]";
 
         public MainWindow()
         {
             InitializeComponent();
 
+            InsertDayCommand = new DelegateCommand(OnInsertDay, CanExecuteCommand);
+            InsertTaskCommand = new DelegateCommand(OnInsertTask, CanExecuteCommand);
+
             LoadFile(Settings.Default.LastPath);
+
+            DataContext = this;
+        }
+
+        private void OnInsertDay(object commandParameter)
+        {
+            AddText(GetDate(), 3);
+        }
+
+        private void OnInsertTask(object commandParameter)
+        {
+            AddText(GetTask());
+        }
+
+        private bool CanExecuteCommand(object commandParameter)
+        {
+            return true;
+        }
+
+        private void AddText(string text, int newLines = 1)
+        {
+            for (int i = 0; i < newLines; i++)
+            {
+                dataTextBox.AppendText(Environment.NewLine);
+            }
+            
+            dataTextBox.AppendText(text);
+            FocusTextBox(dataTextBox);
+        }
+
+        private void FocusTextBox(TextBox textBox)
+        {
+            textBox.CaretIndex = textBox.Text.Length;
+            textBox.ScrollToEnd();
+            textBox.Focus();
+        }
+
+        private string GetDate()
+        {
+            return DateTime.Today.ToString("[dd-MM-yyyy]");
+        }
+
+        private string GetTask()
+        {
+            //- [11:00-11:30] worked on Activity Tracker WPF
+            return DateTime.Now.ToString("- [HH:mm-HH:mm] ");
+
         }
 
         private void LoadFile(string path)
